c7d98e9f1edc6403cf2fa6b5b740c17228a649fd
Shaun Kaasten <shaun@circleci.com>
2023-07-10 15:16:02 -0400

    Make test fail

--- main_test.go
+++ main_test.go
@@ -37,7 +37,7 @@ func Test_diff(t *testing.T) {
 		args args
 		want int
 	}{
-		{"passing", args{2, 1}, 1},
+		{"failing", args{2, 1}, 0},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
