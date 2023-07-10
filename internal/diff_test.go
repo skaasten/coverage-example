c8cacc824a513f8d11516cade95e2f2a1f3404b7
Shaun Kaasten <shaun@circleci.com>
2023-07-10 15:57:11 -0400

    Cache coverage info

--- internal/diff_test.go
+++ internal/diff_test.go
@@ -12,7 +12,7 @@ func Test_diff(t *testing.T) {
 		args args
 		want int
 	}{
-		{"fail", args{2, 1}, 0},
+		{"pass", args{2, 1}, 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
