27ec0a12ca6bac6901b00b35e893da3196a35b89
Shaun Kaasten <shaun@circleci.com>
2023-07-11 10:01:24 -0400

    Simplify tests

--- internal/diff_test.go
+++ internal/diff_test.go
@@ -7,23 +7,10 @@ import (
 )
 
 func Test_diff(t *testing.T) {
-	type args struct {
-		x int
-		y int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{"pass", args{3, 2}, rand.Intn(2)},
-	}
-	for _, tt := range tests {
-		rand.Seed(time.Now().UnixNano())
-		t.Run(tt.name, func(t *testing.T) {
-			if got := diff(tt.args.x, tt.args.y); got != tt.want {
-				t.Errorf("diff() = %v, want %v", got, tt.want)
-			}
-		})
+	rand.Seed(time.Now().UnixNano())
+	expected := rand.Intn(2)
+	got := diff(3, 2)
+	if got != expected {
+		t.Errorf("diff() = %v, want %v", got, expected)
 	}
 }
--- main_test.go
+++ main_test.go
@@ -5,24 +5,9 @@ import (
 )
 
 func Test_sum(t *testing.T) {
-	type args struct {
-		x int
-		y int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-
-		{"passing", args{1, 2}, 3},
-		{"passing-again", args{1, 3}, 4},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := sum(tt.args.x, tt.args.y); got != tt.want {
-				t.Errorf("sum() = %v, want %v", got, tt.want)
-			}
-		})
+	expected := 4
+	got := sum(1, 3)
+	if expected != got {
+		t.Errorf("sum() = %v, want %v", got, expected)
 	}
 }
